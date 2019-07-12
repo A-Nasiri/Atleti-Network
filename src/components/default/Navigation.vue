7f5b57c7977537df90066f1d44ce3c67e697c02c
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2019-07-12 19:49:13 +0100

    Dynamic links and auth functions for Navigation

--- src/components/default/Navigation.vue
+++ src/components/default/Navigation.vue
@@ -13,51 +13,143 @@
       </router-link>
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-btn flat>
+        <v-btn flat v-if="user">
+          <v-icon left color="indigo">mdi-emoticon-happy-outline</v-icon>
+          <span>Hey, {{ userProfile.username }}</span>
+        </v-btn>
+        <v-btn flat class="hidden-xs-only">
+          <v-icon left color="indigo">mdi-newspaper</v-icon>
           <span>News</span>
         </v-btn>
-        <v-btn flat router :to="{ name: 'Register' }">
+        <v-btn v-if="!user" flat router :to="{ name: 'Register' }" class="hidden-xs-only">
           <v-icon left color="indigo">mdi-account-plus</v-icon>
           <span>Signup</span>
         </v-btn>
-        <v-btn flat router :to="{ name: 'Login' }">
+        <v-btn v-if="!user" flat router :to="{ name: 'Login' }" class="hidden-xs-only">
           <v-icon left color="indigo">mdi-login</v-icon>
           <span>Login</span>
         </v-btn>
-        <v-btn flat @click="logOut">
+        <v-btn v-if="user" flat @click="logOut" class="hidden-xs-only">
           <v-icon left color="indigo">mdi-logout</v-icon>
           <span>Logout</span>
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
 
-    <v-navigation-drawer app v-model="drawer" class="drawer">
-      <v-layout
-        column
-        justify-center
-        align-center
-        class="text-xl-center text-lg-center text-md-center text-sm-center text-xs-center"
-      >
-        <v-flex class="mt-5">
-          <v-avatar size="100">
-            <img src="https://randomuser.me/api/portraits/men/85.jpg" />
-          </v-avatar>
-          <p class="white--text subheading mt-1">Diego</p>
-        </v-flex>
-      </v-layout>
+    <v-navigation-drawer v-model="drawer" app dark class="teal">
+      <v-list>
+        <v-layout
+          v-if="user"
+          column
+          justify-center
+          align-center
+          class="text-xl-center text-lg-center text-md-center text-sm-center text-xs-center"
+        >
+          <v-flex class="mt-5">
+            <v-avatar size="100">
+              <img :src="userProfile.image_url" />
+            </v-avatar>
+            <p class="white--text subheading mt-1">{{ userProfile.username }}</p>
+          </v-flex>
+        </v-layout>
+        <v-divider></v-divider>
+        <v-list-tile @click="goHome">
+          <v-list-tile-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>HOME</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile v-if="!user" @click="goSignup">
+          <v-list-tile-action>
+            <v-icon>mdi-account-plus</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>SIGN UP</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile v-if="!user" @click="goLogin">
+          <v-list-tile-action>
+            <v-icon>mdi-login</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>LOG IN</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>mdi-newspaper</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>NEWS</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile v-if="user" @click="goMap">
+          <v-list-tile-action>
+            <v-icon>mdi-earth</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>ATLETI-MAP</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile v-if="user" @click="goMap">
+          <v-list-tile-action>
+            <v-icon>mdi-wall</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>PROFILE</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile v-if="user" @click="logOut">
+          <v-list-tile-action>
+            <v-icon>mdi-logout</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>LOG OUT</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
 import firebase from "firebase/app";
+import db from "@/firebase/init";
 
 export default {
   name: "Navigation",
   data() {
     return {
-      drawer: false
+      drawer: false,
+      user: null,
+      userProfile: null
     };
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.user = user;
+        db.collection("users")
+          .where("user_id", "==", user.uid)
+          .get()
+          .then(snapshot => {
+            snapshot.forEach(doc => {
+              this.userProfile = doc.data();
+            });
+          });
+      } else {
+        this.user = null;
+      }
+    });
   },
   methods: {
     logOut() {
@@ -67,6 +159,18 @@ export default {
         .then(() => {
           this.$router.push({ name: "Login" });
         });
+    },
+    goHome() {
+      this.$router.push({ name: "Home" });
+    },
+    goLogin() {
+      this.$router.push({ name: "Login" });
+    },
+    goSignup() {
+      this.$router.push({ name: "Register" });
+    },
+    goMap() {
+      this.$router.push({ name: "Gmap" });
     }
   }
 };
@@ -91,7 +195,22 @@ export default {
   width: 107px;
 }
 
-.drawer {
+.draw {
   background-image: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+}
+
+@media only screen and (max-width: 600px) {
+  .lg {
+    width: 40px;
+    margin: 0 0.3rem;
+  }
+
+  .logo .atl {
+    width: 50px;
+  }
+
+  .logo .net {
+    width: 75px;
+  }
 }
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -86,9 +86,7 @@ export default {
       //
     };
   },
-  mounted() {
-    console.log(firebase.auth().currentUser);
-  }
+  mounted() {}
 };
 </script>
 
